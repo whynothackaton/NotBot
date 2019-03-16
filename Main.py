da96b1d763a5657cb919e2f774152482ac345c9b
Denis Medvedev <medvedev0denis@gmail.com>
2019-03-16 15:05:00 +0400

    Deploy

--- Main.py
+++ Main.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, Response
-from SocialNetworks.ViberNetwork import ViberNetwork
-from SocialNetworks.VKNetwork import VKNetwork
+from NotBot.ViberNetwork import ViberNetwork
+from NotBot.VKNetwork import VKNetwork
 from viberbot.api.bot_configuration import BotConfiguration
 import json
 
 app = Flask(__name__)
-bot_configuration = BotConfiguration(name='NotBot',avatar='http://viber.com/avatar.jpg',auth_token='49615ef8ed67d5b9-50f2f0eb06ccc34a-8f1ed6e0cf602b98')
-vn=ViberNetwork(bot_configuration)
+#bot_configuration = BotConfiguration(name='NotBot',avatar='http://viber.com/avatar.jpg',auth_token='49615ef8ed67d5b9-50f2f0eb06ccc34a-8f1ed6e0cf602b98')
+#vn=ViberNetwork(bot_configuration)
+print("WORK")
 
 @app.route('/', methods=['POST'])
 def incoming():
     data = json.loads(request.data)
-    
+      
     if data['type'] == 'confirmation':
         return '18258778'
     return Response(status=200)
