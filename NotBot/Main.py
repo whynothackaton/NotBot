c3ec072610183058052fc96f9ba24770d7bb2aa1
Denis Medvedev <medvedev0denis@gmail.com>
2019-03-16 14:37:07 +0400

    1

--- NotBot/Main.py
+++ NotBot/Main.py
@@ -15,6 +15,6 @@ def incoming():
         return '18258778'
     return Response(status=200)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=443, debug=True)
+
+app.run(host='0.0.0.0', port=443, debug=True)
     
