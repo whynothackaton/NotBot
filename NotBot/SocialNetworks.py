f8bc7942012641d9af7583fbdedf2afba846d937
Denis Medvedev <medvedev0denis@gmail.com>
2019-03-16 17:22:15 +0400

    Auth
    3244

--- NotBot/SocialNetworks.py
+++ NotBot/SocialNetworks.py
@@ -1,11 +1,10 @@
 from .VKNetwork import VKNetwork
-from .ViberNetwork import TGNetwork
 
 
 class SocialNetworks():
     def __init__(self):
         self.VK_bot=VKNetwork(vk_access_token='66d88a983bd5b7f39de9db6f9235782db2b62a03160978a44c9ebc4e97558a335e7b1bf32317203799a5d',group_id=179748337,vk_api_version='5.92')
-        self.TG_bot=TGNetwork('898225498:AAHUIh-SHjcQr1XdDPu4V1xWPghw6kx6__E')
+        
 
     def send(self,message,id,method):
         if method=='vk':
