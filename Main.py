86b01b644bf054297277dc33c6b648b763525710
Denis Medvedev <medvedev0denis@gmail.com>
2019-03-16 17:15:38 +0400

    Auth

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response
 from NotBot.SocialNetworks import SocialNetworks
 from viberbot.api.bot_configuration import BotConfiguration
+from MailBox import MailBox
 import json
 import os
 import threading
@@ -8,7 +9,7 @@ import time
 
 app = Flask(__name__)
 
-
+MBoxs=[]
 sn= SocialNetworks()
 
 @app.route('/', methods=['POST'])
@@ -23,12 +24,23 @@ def incoming():
         sn.send(id=sn.user_id,message="Hello",method='vk')
     return Response(status=200)
 
+@app.route('/login', methods=['POST'])
+def login():
+    email_domen=request.form['domen']
+    login=request.form['username']
+    password=request.form['password']
+    MBoxs.append(MailBox(email_domen,login,password))
+    return "OK"
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    return  render_template('login.html')
 
 def Main():
     while True:
         #sn.send(id='207189016',message="12",method='vk')
         time.sleep(15)
-
+        
 
 if __name__ == "__main__":
     thread1 = threading.Thread(target=Main)
--- NotBot/SocialNetworks.py
+++ NotBot/SocialNetworks.py
@@ -1,17 +1,16 @@
 from .VKNetwork import VKNetwork
-from .ViberNetwork import ViberNetwork
-from viberbot.api.bot_configuration import BotConfiguration
+from .ViberNetwork import TGNetwork
+
 
 class SocialNetworks():
     def __init__(self):
         self.VK_bot=VKNetwork(vk_access_token='66d88a983bd5b7f39de9db6f9235782db2b62a03160978a44c9ebc4e97558a335e7b1bf32317203799a5d',group_id=179748337,vk_api_version='5.92')
-        bot_configuration = BotConfiguration(name='NotBot',avatar='https://pp.userapi.com/c850332/v850332246/105af0/pTe6rXEBU9E.jpg',auth_token='49615ef8ed67d5b9-50f2f0eb06ccc34a-8f1ed6e0cf602b98')
-        self.Viber_bot=ViberNetwork(bot_configuration)
+        self.TG_bot=TGNetwork('898225498:AAHUIh-SHjcQr1XdDPu4V1xWPghw6kx6__E')
 
     def send(self,message,id,method):
         if method=='vk':
             self.VK_bot.send_message(id,message)
-        if method=='viber':
-            self.Viber_bot.send_message(id,message)
+        if method=='tg':
+            self.TG_bot.send_message(id,message)
     def set_user(self,id):
         self.user_id=id
