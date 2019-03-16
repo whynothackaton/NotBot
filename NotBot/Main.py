86f4069fad8015ada7d9b57fd101090f2f7a8c21
Denis Medvedev <medvedev0denis@gmail.com>
2019-03-16 15:04:06 +0400

    Deploy

--- NotBot/Main.py
+++ NotBot/Main.py
@@ -5,13 +5,14 @@ from viberbot.api.bot_configuration import BotConfiguration
 import json
 
 app = Flask(__name__)
-bot_configuration = BotConfiguration(name='NotBot',avatar='http://viber.com/avatar.jpg',auth_token='49615ef8ed67d5b9-50f2f0eb06ccc34a-8f1ed6e0cf602b98')
-vn=ViberNetwork(bot_configuration)
+#bot_configuration = BotConfiguration(name='NotBot',avatar='http://viber.com/avatar.jpg',auth_token='49615ef8ed67d5b9-50f2f0eb06ccc34a-8f1ed6e0cf602b98')
+#vn=ViberNetwork(bot_configuration)
+print("WORK")
 
 @app.route('/', methods=['POST'])
 def incoming():
     data = json.loads(request.data)
-    
+      
     if data['type'] == 'confirmation':
         return '18258778'
     return Response(status=200)
