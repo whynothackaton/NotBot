a27ecf7f88a7f3f61adf9c6dc50146c633c22177
Denis Medvedev <medvedev0denis@gmail.com>
2019-03-16 14:02:31 +0400

    deploy

--- NotBot/SocialNetworks/VKNetwork.py
+++ NotBot/SocialNetworks/VKNetwork.py
@@ -8,31 +8,4 @@ class VKNetwork():
         self.group_id=group_id
 
 
-    def connect_to_long_poll(self):
-        try:
-            self.longPoll = self.vk_api.groups.getLongPollServer(group_id=self.group_id)
-            self.server = self.longPoll['server']
-            self.key = self.longPoll['key']
-            self.ts = self.longPoll['ts']
-
-        except Exception as e:
-            error = "Error with connect to vk longPoll: " + str(e)
-            print(error)
-
-    def vk_run(self):
-        try:
-                self.longPoll = post(
-                    '%s' % self.server,
-                    data={
-                        'act': 'a_check',
-                        'key': self.key,
-                        'ts': self.ts,
-                        'wait': 25
-                        }).json()
-
-                    if self.longPoll['updates'] and len(self.longPoll['updates']) != 0:
-                        for update in self.longPoll['updates']:
-                        if update['type'] == 'message_new':
-                            print("Q")
-        except Exception as e:
-            print(e)
+    def send_message(self):
