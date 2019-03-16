116dd41db37f9729416cf14d3b231c05e3060a76
Denis Medvedev <medvedev0denis@gmail.com>
2019-03-16 16:17:25 +0400

    fix

--- NotBot/ViberNetwork.py
+++ NotBot/ViberNetwork.py
@@ -14,7 +14,7 @@ class ViberNetwork():
     def __init__(self,bot_configuration):
         self.bot_configuration=bot_configuration
         self.viber=Api(self.bot_configuration)
-        self.viber.set_webhook('https://notbotme.herokuapp.com/:'+str(int(os.environ.get("PORT", 5000))))
+        
     def send_message(self,message,id):
         self.viber.send_messages(id,message)
 
