ccdda8da95fe2a328a5e0cabf3ed57ea42b803ff
Andrey Naidenko <naidenkoandrey1997@mail.ru>
2019-05-16 19:19:31 +0400

    get token, get last message

--- MailBox.py
+++ MailBox.py
@@ -5,32 +5,35 @@ import datetime
 
 
 class MailBox:
-    def __init__(self):
-        self.email_domen = None
-        self.login = None
-        self.password = None
+    def __init__(self, email):
+        self.email = email       
         self.imap = None
+        self.id = '3ee652c711e9455c98afa34a2807e4f3'
 
-    def __init__(self, email_domen, login, password,id):
-        self.email_domen = email_domen
-        self.login = login
-        self.password = password
-        self.imap = None
-        self.id=id
+    def connection(self, token):
+        token = 'AgAAAAAnAbXhAAWqhbsqAzRd1UPSr3nUk_xugDY'
 
-    def connection(self):
-        self.imap = imaplib.IMAP4_SSL('imap.' + self.email_domen)
-        self.imap.login(self.login + '@' + self.email_domen, self.password)     
-        print('auth')   
-    
-    def get_new_message(self, date_time): 
+        auth_string = 'user={0}\1auth=Bearer {1}\1\1' \
+            .format(self.email, token)
+        self.imap = imaplib.IMAP4_SSL('imap.yandex.com')
+        self.imap.authenticate('XOAUTH2', lambda x: auth_string)
+  
+    def get_new_message(self): 
         status, message = self.imap.select('INBOX')
         assert status == 'OK'
-       # result, data = self.imap.search(None, 'ALL')
 
+        '''        
         date = (datetime.date.today() - datetime.timedelta(1)).strftime("%d-%b-%Y")
         result, data = self.imap.uid('search', None, '(SENTSINCE {date})'.format(date=date))        
         
+        ids = data[0] # data is a list.
+        id_list = ids.split() # ids is a space separated string
+        latest_email_id = id_list[-1] # get the latest
+        
+        result, data = self.imap.uid('fetch', latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+        '''
+
+        result, data = self.imap.search(None, 'ALL')
         ids = data[0] # data is a list.
         id_list = ids.split() # ids is a space separated string
         latest_email_id = id_list[-1] # get the latest
@@ -45,15 +48,14 @@ class MailBox:
             local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
             local_message_date = "%s" %(str(local_date.strftime("%a, %d %b %Y %H:%M:%S")))
         
-
         email_from = str(email.header.make_header(email.header.decode_header(email_message['From'])))
         email_to = str(email.header.make_header(email.header.decode_header(email_message['To'])))
         subject = str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
 
-        if local_date > date_time:
-            return email_from + '\n ' + subject
-        else:
-            None
+        #if local_date > date_time:
+        return email_from + '\n ' + subject
+        #else:
+        #    None
 
     def close_connection(self):
         self.imap.close()
--- Main.py
+++ Main.py
@@ -7,15 +7,18 @@ import datetime
 
 app = Flask(__name__)
 
-
-
-@app.route('/email=<token>',methods=['GET', 'POST'])
+@app.route('access_token/=<token>', methods=['GET', 'POST'])
 def incoming(token):
-    return token
+    mb.connection(token)
+    new_message = mb.get_new_message()
+    if new_message is not None:
+        pass # отправляем new_message в вк
+    mb.close_connection()
         
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    mb = MailBox('rollabushka@yandex.ru')        
 
     
