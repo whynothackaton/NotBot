eab266004d86a22cdd818c249df26d58f1a099eb
Andrey Naidenko <naidenkoandrey1997@mail.ru>
2019-05-17 14:33:16 +0400

    Update MailBox.py

--- MailBox.py
+++ MailBox.py
@@ -12,7 +12,6 @@ class MailBox:
         self.id = '3ee652c711e9455c98afa34a2807e4f3'
 
     def connection(self, token):
-        token = 'AgAAAAAnAbXhAAWqhbsqAzRd1UPSr3nUk_xugDY'
         auth_string = 'user={0}\1auth=Bearer {1}\1\1' \
             .format(self.email, token)
         self.imap = imaplib.IMAP4_SSL('imap.yandex.com')
@@ -21,29 +20,29 @@ class MailBox:
     def get_new_letter(self): 
         status, letter = self.imap.select('INBOX')
 
-        assert status == 'OK'              
-        date = (datetime.date.today() - datetime.timedelta(1)).strftime("%d-%b-%Y")
-        print(datetime.date.today())
-        print(datetime.timedelta(1))
-        print(date)
-        status, data = self.imap.uid('search', None, '(SENTSINCE {date})'.format(date=date)) 
+        #assert status == 'OK'              
+        #date = (datetime.date.today() - datetime.timedelta(1)).strftime("%d-%b-%Y")
+        #print(datetime.date.today())
+        #print(datetime.timedelta(1))
+        #print(date)
+        #status, data = self.imap.uid('search', None, '(SENTSINCE {date})'.format(date=date)) 
         
         assert status == 'OK'     
         ids = data[0] # data is a list.
         id_list = ids.split() # ids is a space separated string
         latest_email_id = id_list[-1] # get the latest
         # fetch the email body () for the given ID
-        #status, data = self.imap.uid('fetch', latest_email_id, "(RFC822)")         
-        result, data = self.imap.search(None, 'ALL')
+        status, data = self.imap.uid('fetch', latest_email_id, "(RFC822)")         
+        #result, data = self.imap.search(None, 'ALL')
         
-        print('status', status)
+        #print('status', status)
         assert status == 'OK'        
         letter = self.parse_letter(data)
         
-        if local_date > date_time:
-            return letter
-        else:
-            None
+        #if local_date > date_time:
+        return letter
+        #else:
+        #    None
         
     def parse_letter(self, data):        
         raw_email = data[0][1]
