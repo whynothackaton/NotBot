64cf20eeebb3c9299eac74cbe261ae692fa8eba4
Denis Medvedev <medvedev0denis@gmail.com>
2019-05-21 20:22:54 +0400

    Update pai.py

--- pai.py
+++ pai.py
@@ -23,8 +23,8 @@ class PaiFlow():
         b = b.lower()
 
         pattern = re.compile('<[a-z0-9]*>')
-        a_re = re.search(pattern)
-        b_re = re.search(pattern)
+        a_re = re.search(a,pattern)
+        b_re = re.search(b,pattern)
         if a_re or b_re:
             return SequenceMatcher(None, a_re.group(), b_re.group()).ratio()
         return SequenceMatcher(None, a.group(), b.group()).ratio()
