1099d01e733124a4d97e5986b700990aa403e71a
Denis Medvedev <medvedev0denis@gmail.com>
2019-05-21 17:26:30 +0400

    Update Registrar.py

---
+++ Registrar.py
@@ -0,0 +1,28 @@
+class Registrar (object):
+    def __init__(self):
+        self.commands = {}
+
+    def add(self, *args, **kwargs):
+        '''
+        kwargs
+            category[str]-Category
+        '''
+        print("ARGS1=",args)
+        def add(command):
+            if kwargs['category'] in self.commands:
+                self.commands[kwargs['category']].append(command)
+            else:
+                self.commands[kwargs['category']] = [command]
+
+        return add
+
+    def execute(self, *args, **kwargs):
+        '''[summary]
+
+        Keyword Arguments:
+            category {[type]} -- [description] (default: {None})
+        '''
+        print("ARGS2=",args)
+        category = kwargs['category']
+        for command in self.commands[category]:
+            command(args, kwargs)
