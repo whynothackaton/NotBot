9c0fa31daa0cef6764726bf63b35dd3e9e8f061a
Denis Medvedev <37630193+MedvedevDenis@users.noreply.github.com>
2019-05-26 19:40:37 +0400

    Bug fix

--- Main.py
+++ Main.py
@@ -99,11 +99,12 @@ def incoming_mail():
             'redirect_uri': 'https://notbotme.herokuapp.com/mail_auth'
         }
 
-        response = requests.post(url=url, data=data))  
-        
+        response = requests.post(url=url, data=data)
+
         return response.text
 
     return redirect('/')
+
 
 @app.route('/bot', methods=['POST'])
 def botserver():
